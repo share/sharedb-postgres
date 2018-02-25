7becba960fa24e211d5e03caa2273838da95d9fc
William Claydon <will@heisamachine.com>
2018-02-25 11:36:34 +1100

    reverted es6 convertion

--- README.md
+++ README.md
@@ -3,10 +3,9 @@
 Enhanced PostgreSQL database adapter for [sharedb](https://github.com/share/sharedb). This
 driver can be used both as a snapshot store and oplog.
 
-Doesn't support queries (yet?).
+This a fork of [sharedb-postgres](https://github.com/share/sharedb-postgres) that should fix the [high concurency issues](https://github.com/share/sharedb-postgres/issues/1) by updating the snapshot and inserting the operation in the same transcation. In order to better impletment queries in the future, the postgres JSONB datatype is used instead. As a result of this you now need Postgres 9.5+.
 
-This a fork of [sharedb-postgres](https://github.com/share/sharedb-postgres) that should fix the [high concurency issues](https://github.com/share/sharedb-postgres/issues/1), rewrote to be in js es6 and now uses JSONB datatype in stead. As a result of this you now need Postgres 9.5+.
-
+Also note, this now uses a pool of database conections instead. 
 
 ## Usage
 
--- index.js
+++ index.js
@@ -3,35 +3,38 @@ var pg = require('pg');
 
 // Postgres-backed ShareDB database
 
-module.exports = class PostgresDB {
-  constructor(options) {
-    DB.call(this, options);
+function PostgresDB(options) {
+  if (!(this instanceof PostgresDB)) return new PostgresDB(options);
+  DB.call(this, options);
 
-    this.closed = false;
+  this.closed = false;
 
-    this.pg_config = options;
+  this.pg_config = options;
     this.pool = new pg.Pool(this.pg_config)
+};
+module.exports = PostgresDB;
 
-  }
+PostgresDB.prototype = Object.create(DB.prototype);
 
-  close(callback) {
-    this.closed = true;
-    if (callback) callback();
-  }
+PostgresDB.prototype.close = function(callback) {
+  this.closed = true;
+  if (callback) callback();
+};
 
-  // Persists an op and snapshot if it is for the next version. Calls back with
-  // callback(err, succeeded)
-  commit(collection, id, op, snapshot, options, callback) {
-    /*
-     * op: CreateOp {
-     *   src: '24545654654646',
-     *   seq: 1,
-     *   v: 0,
-     *   create: { type: 'http://sharejs.org/types/JSONv0', data: { ... } },
-     *   m: { ts: 12333456456 } }
-     * }
-     * snapshot: PostgresSnapshot
-     */
+
+// Persists an op and snapshot if it is for the next version. Calls back with
+// callback(err, succeeded)
+PostgresDB.prototype.commit = function(collection, id, op, snapshot, options, callback) {
+  /*
+   * op: CreateOp {
+   *   src: '24545654654646',
+   *   seq: 1,
+   *   v: 0,
+   *   create: { type: 'http://sharejs.org/types/JSONv0', data: { ... } },
+   *   m: { ts: 12333456456 } }
+   * }
+   * snapshot: PostgresSnapshot
+   */
     this.pool.connect((err, client, done) => {
       if (err) {
         done(client);
@@ -60,100 +63,101 @@ module.exports = class PostgresDB {
         console.log(err.stack)
         callback(err)
       } else if(res.rows.length === 0) {
+        done(client);
         console.log("Unable to commit, not the latest version")
         callback(null,false)
       } 
       else {
+        done(client);
         console.log(res.rows[0])
         callback(null,true)
       }
     })
     
     })
-  }
+};
 
-  // Get the named document from the database. The callback is called with (err,
-  // snapshot). A snapshot with a version of zero is returned if the docuemnt
-  // has never been created in the database.
-  getSnapshot(collection, id, fields, options, callback) {
-    this.pool.connect((err, client, done) => {
-      if (err) {
-        done(client);
-        callback(err);
-        return;
-      }
-      client.query(
-        'SELECT version, data, doc_type FROM snapshots WHERE collection = $1 AND doc_id = $2 LIMIT 1',
-        [collection, id],
-        (err, res) => {
-          done();
-          if (err) {
-            callback(err);
-            return;
-          }
-          if (res.rows.length) {
-            const row = res.rows[0];
-            var snapshot = new PostgresSnapshot(
-              id,
-              row.version,
-              row.doc_type,
-              row.data,
-              undefined // TODO: metadata
-            )
-            callback(null, snapshot);
-          } else {
-            var snapshot = new PostgresSnapshot(
-              id,
-              0,
-              null,
-              undefined,
-              undefined
-            )
-            callback(null, snapshot);
-          }
+// Get the named document from the database. The callback is called with (err,
+// snapshot). A snapshot with a version of zero is returned if the docuemnt
+// has never been created in the database.
+PostgresDB.prototype.getSnapshot = function(collection, id, fields, options, callback) {
+  this.pool.connect(function(err, client, done) {
+    if (err) {
+      done(client);
+      callback(err);
+      return;
+    }
+    client.query(
+      'SELECT version, data, doc_type FROM snapshots WHERE collection = $1 AND doc_id = $2 LIMIT 1',
+      [collection, id],
+      function(err, res) {
+        done();
+        if (err) {
+          callback(err);
+          return;
         }
-      )
-    })
-  }
-
-  // Get operations between [from, to) noninclusively. (Ie, the range should
-  // contain start but not end).
-  //
-  // If end is null, this function should return all operations from start onwards.
-  //
-  // The operations that getOps returns don't need to have a version: field.
-  // The version will be inferred from the parameters if it is missing.
-  //
-  // Callback should be called as callback(error, [list of ops]);
-  getOps(collection, id, from, to, options, callback) {
-    this.pool.connect(  (err, client, done) => {
-      if (err) {
-        done(client);
-        callback(err);
-        return;
-      }
-      client.query(
-        'SELECT version, operation FROM ops WHERE collection = $1 AND doc_id = $2 AND version >= $3 AND version < $4',
-        [collection, id, from, to],
-        (err, res) => {
-          done();
-          if (err) {
-            callback(err);
-            return;
-          }
-          callback(null, res.rows.map(row => row.operation));
+        if (res.rows.length) {
+          var row = res.rows[0]
+          var snapshot = new PostgresSnapshot(
+            id,
+            row.version,
+            row.doc_type,
+            row.data,
+            undefined // TODO: metadata
+          )
+          callback(null, snapshot);
+        } else {
+          var snapshot = new PostgresSnapshot(
+            id,
+            0,
+            null,
+            undefined,
+            undefined
+          )
+          callback(null, snapshot);
         }
-      )
-    })
-  }
-}
+      }
+    )
+  })
+};
 
-class PostgresSnapshot {
-  constructor(id, version, type, data, meta) {
-    this.id = id;
-    this.v = version;
-    this.type = type;
-    this.data = data;
-    this.m = meta;
-  }
+// Get operations between [from, to) noninclusively. (Ie, the range should
+// contain start but not end).
+//
+// If end is null, this function should return all operations from start onwards.
+//
+// The operations that getOps returns don't need to have a version: field.
+// The version will be inferred from the parameters if it is missing.
+//
+// Callback should be called as callback(error, [list of ops]);
+PostgresDB.prototype.getOps = function(collection, id, from, to, options, callback) {
+  this.pool.connect(function(err, client, done) {
+    if (err) {
+      done(client);
+      callback(err);
+      return;
+    }
+    client.query(
+      'SELECT version, operation FROM ops WHERE collection = $1 AND doc_id = $2 AND version >= $3 AND version < $4',
+      [collection, id, from, to],
+      function(err, res) {
+        done();
+        if (err) {
+          callback(err);
+          return;
+        }
+        callback(null, res.rows.map(function(row) {
+          return row.operation;
+        }));
+      }
+    )
+  })
+};
+
+function PostgresSnapshot(id, version, type, data, meta) {
+  this.id = id;
+  this.v = version;
+  this.type = type;
+  this.data = data;
+  this.m = meta;
 }
